142db36c1bfce1ef682f1b3638a3e7b6859b5402
Morris Kim <morris.kim@binus.ac.id>
2021-06-07 09:45:02 +0700

    added assignment

--- Week 10/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Main {
-    public static void main(String args[]){
-        PoliceCar policeCar = new PoliceCar();
-        PoliceCar[] myList = {policeCar};
-        myList[0].chase();
-    }
-}
